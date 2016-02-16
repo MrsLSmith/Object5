cb8eaedcb92c989d0bf6c52b02d2271548fa01fd
Smith <lsmith@ccsuvt.org>
2016-02-15 20:23:10 -0500

    Added array

---
+++ superheroes.js
@@ -0,0 +1,13 @@
+var superheroes = [{
+  Name: "Incredible Hulk",
+  Color: "Green",
+  Superpower: "Strength"
+}, {
+  Name: "Superman",
+  Color: "Blue",
+  Superpower: "Flying"
+}, {
+  Name: "Hawkeye",
+  Color: "Purple",
+  Superpower: "Archery"
+}];
