45162a32a4c9478fb2b40917523ec45d71763541
NLampron <NLampron@ccsuvt.org>
2016-03-08 09:29:51 -0500

    Finished Name Function

--- superheroes.js
+++ superheroes.js
@@ -11,3 +11,9 @@ var superheroes = [{
   Color: "Purple",
   Superpower: "Archery"
 }];
+
+function listHeroes() {
+  for (var i = 0; i < 3; i++) {
+ document.getElementById('results').innerHTML += superheroes[i].Name + "<p></p>";
+  }
+}
