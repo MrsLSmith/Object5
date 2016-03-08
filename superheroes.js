7cca50173d0c20287cf79a9ea44496c39a62f648
Ken <sliu@ccsuvt.org>
2016-03-08 10:15:23 -0500

    fixed some of the printing errors with my group!

--- superheroes.js
+++ superheroes.js
@@ -11,10 +11,26 @@ var superheroes = [{
   Color: "Purple",
   Superpower: "Archery"
 }];
+
 function showTable() {
   for(var i=0; i < 3; i++) {
-    document.getElementById('results').innerHTML += "<tr>";
-    document.getElementById('results').innerHTML += "<td>" + superheroes[i].Name + "</td>" + "<td>" + superheroes[i].Color + "</td>" + "<td>"+ superheroes[i].Superpower + "</td>";
-    document.getElementById('results').innerHTML += "</tr>";
+    document.getElementById('results2').innerHTML += "<tr>";
+    document.getElementById('results2').innerHTML += "<td>" + superheroes[i].Name + "</td>" + "<td>" + superheroes[i].Color + "</td>" + "<td>"+ superheroes[i].Superpower + "</td>";
+    document.getElementById('results2').innerHTML += "</tr>";
   }
 };
+
+function listHeroes() {
+  for (var i = 0; i < 3; i++) {
+ document.getElementById('results1').innerHTML += superheroes[i].Name + "<p></p>";
+  }
+};
+
+function listHeadings() {
+  var headings = superheroes[0];
+  var list = "";
+  for (var key in headings) {
+   list += "<p>" + key + "</p>";
+  }
+  document.getElementById('headings').innerHTML = list;
+}
